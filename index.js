54c7f4a873731ab119d8a16ae2ddd3137afe89bf
Shaun Rezac <shaun.rezac@gmail.com>
2018-04-24 14:33:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays() {
   return 'Happy holidays!'
 }
 
-function happyHolidaysTo(you) {
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
