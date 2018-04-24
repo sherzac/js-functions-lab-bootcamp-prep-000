243416aba239c167cb70ff4e8bc78dfb4a2dbc8f
Shaun Rezac <shaun.rezac@gmail.com>
2018-04-24 14:10:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ function happyHolidaysTo(name) {
   return 'Happy holidays, you!'
 }
 
+holiday = 'Independence day'
 function happyHolidayTo(holiday, name) {
   return 'Happy Independence Day, you!'
 }
 
+holiday = "Mother's Day"
+days = 20
 function holidayCountdown (holiday, days) {
   return 'Its 20 days until Mothers day!'
 }
