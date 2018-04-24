898106f4fbf90a54e895844ae3c1d2d0a721a1fa
Shaun Rezac <shaun.rezac@gmail.com>
2018-04-24 14:15:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown (holiday, days) {
-  return "It's 20 days until Mother's day!"
+  return "It's 20 days until Mother's Day!"
 }
